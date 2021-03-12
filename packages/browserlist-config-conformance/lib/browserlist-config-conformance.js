7866609a3f9d58d55de109e2b4789e57a7b80c22
sam <sam@freighttrust.com>
2021-03-12 01:34:02 -0800

    feat(packages): config-libs

---
+++ packages/browserlist-config-conformance/lib/browserlist-config-conformance.js
@@ -0,0 +1,4 @@
+// @file browserlist config
+'use strict';
+
+module.exports = ['last 1 version', 'ie >= 11', 'Firefox ESR'];
---
+++ packages/jest-config-conformance/lib/transform/jsTransform.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { createTransformer } = require('babel-jest');
+
+const babelOptions = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['extends browserslist-config-carbon'],
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
+  ],
+};
+
+module.exports = createTransformer(babelOptions);
